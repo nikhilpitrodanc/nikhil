87db85383a759d2d0d6a1da0c021ad66757012a2
Nikhil Pitroda <nikhilpitrodanc@gmail.com>
2025-01-18 08:59:36 +0530

    show main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+a='Nikhil'
+b='nikhil'
+print(a<b)
+c=100
+d=100
+print(c==d)
+print(c!=d)
+print(a.casefold() is b)
+print(b.casefold() == a)
+x = 100 if c>d else d
+print(x)
+
+
+# implementing module
+# import modulename
+# from modulename import *
